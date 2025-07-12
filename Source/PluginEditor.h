0e6bafd5c20e94643acd3aff3eb478301ee1658e
Roei Meiri <roei.meiri9@hotmail.co.il>
2025-07-12 17:01:29 +0300

    Added Sliders to GUI

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,7 +1,7 @@
 /*
   ==============================================================================
 
-    This file contains the basic framework code for a JUCE plugin editor.
+	This file contains the basic framework code for a JUCE plugin editor.
 
   ==============================================================================
 */
@@ -11,23 +11,41 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
+struct CustomRotarySlider: juce::Slider {
+	CustomRotarySlider(): juce::Slider(
+		juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+		juce::Slider::TextEntryBoxPosition::NoTextBox
+	) {
+
+	}
+};
+
 //==============================================================================
 /**
 */
-class SimpleEQAudioProcessorEditor : public juce::AudioProcessorEditor
-{
+class SimpleEQAudioProcessorEditor: public juce::AudioProcessorEditor {
 public:
-    SimpleEQAudioProcessorEditor (SimpleEQAudioProcessor&);
-    ~SimpleEQAudioProcessorEditor() override;
+	SimpleEQAudioProcessorEditor(SimpleEQAudioProcessor &);
+	~SimpleEQAudioProcessorEditor() override;
 
-    //==============================================================================
-    void paint (juce::Graphics&) override;
-    void resized() override;
+	//==============================================================================
+	void paint(juce::Graphics &) override;
+	void resized() override;
 
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
-    SimpleEQAudioProcessor& audioProcessor;
+	// This reference is provided as a quick way for your editor to
+	// access the processor object that created it.
+	SimpleEQAudioProcessor &audioProcessor;
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessorEditor)
+	CustomRotarySlider peakFreqSlider,
+		peakGainSlider,
+		peakQualitySlider,
+		lowCutFreqSlider,
+		highCutFreqSlider,
+		lowCutSlopeSlider,
+		highCutSlopeSlider;
+
+	std::vector<juce::Component *> getComps();
+
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SimpleEQAudioProcessorEditor)
 };
